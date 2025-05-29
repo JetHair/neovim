758a3eb355bc168b306dc4bc156ec0539275c507
JetHair <jethair@duck.com>
2025-05-29 16:56:23 +0530

    changed package name to nixCats again

--- flake.nix
+++ flake.nix
@@ -334,7 +334,7 @@
     packageDefinitions = {
       # the name here is the name of the package
       # and also the default command name for it.
-      nvim = { pkgs, name, ... }@misc: {
+      nixCats = { pkgs, name, ... }@misc: {
         # these also recieve our pkgs variable
         # see :help nixCats.flake.outputs.packageDefinitions
         settings = {
@@ -345,7 +345,7 @@
           # or, whatever you named the package definition in the packageDefinitions set.
           # WARNING: MAKE SURE THESE DONT CONFLICT WITH OTHER INSTALLED PACKAGES ON YOUR PATH
           # That would result in a failed build, as nixos and home manager modules validate for collisions on your path
-          aliases = [ "vim" "v"];
+          aliases = [ "vim" "vimcat" "v"];
 
           # explained below in the `regularCats` package's definition
           # OR see :help nixCats.flake.outputs.settings for all of the settings available
